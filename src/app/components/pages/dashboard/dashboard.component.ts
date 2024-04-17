636410310e7b08b16848be65700cc8492f69a86f
Ushmita Dutta <CZ0224@cztech.com>
2024-04-17 16:20:54 +0530

    select search done

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HomePageComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
--- src/app/components/pages/dashboard/home-page/home-page.component.ts
+++ src/app/components/pages/dashboard/home-page/home-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatGridListModule,MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule,NgxMatSelectSearchModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })

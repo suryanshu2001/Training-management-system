6aa4c594ea5ca40ba385f4b8668a3ce777623a9e
hemanth <yamarpuhemanth777@gmail.com>
2024-04-17 13:22:01 +0530

    first commit

---
+++ src/app/components/shared-components/data-entry/data-entry.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-data-entry',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data-entry.component.html',
+  styleUrls: ['./data-entry.component.scss']
+})
+export class DataEntryComponent {
+
+}

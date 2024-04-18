6e6e3668b963504c08a9979b8f7644edd92d5ba7
Ushmita Dutta <CZ0224@cztech.com>
2024-04-18 13:16:51 +0530

    added the component for batches

--- src/app/components/pages/dashboard/home-page/home-page.component.ts
+++ src/app/components/pages/dashboard/home-page/home-page.component.ts
@@ -6,14 +6,20 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { BatchesComponent } from './batches/batches.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule,MatGridListModule,MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule,NgxMatSelectSearchModule],
+  imports: [CommonModule,MatGridListModule,MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule,NgxMatSelectSearchModule,BatchesComponent,MatGridListModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+    tabName: string = 'batchProgram';
 
+    onToggle(tab:string){
+      console.log(tab,'selected');
+      this.tabName = tab;
+    }
 }

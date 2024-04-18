567632b091ef98e9110c5ad9815b0a6204b28d70
hemanth <yamarpuhemanth777@gmail.com>
2024-04-18 17:44:24 +0530

    completed on assignment-header

--- src/app/components/pages/assignments-page/assignments-page.component.ts
+++ src/app/components/pages/assignments-page/assignments-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataEntryComponent } from '../../shared-components/data-entry/data-entry.component';
 
 @Component({
   selector: 'app-assignments-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    DataEntryComponent
+  ],
   templateUrl: './assignments-page.component.html',
   styleUrls: ['./assignments-page.component.scss']
 })
--- src/app/components/pages/exams-page/exams-page.component.ts
+++ src/app/components/pages/exams-page/exams-page.component.ts
@@ -1,45 +1,18 @@
 import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import { FormsModule, NgForm } from '@angular/forms';
-import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
-import {MatGridListModule} from '@angular/material/grid-list';
+
 
 
 @Component({
   selector: 'app-exams-page',
   standalone: true,
-  imports: [CommonModule,MatDatepickerModule,MatInputModule,MatFormFieldModule,MatNativeDateModule,MatIconModule,MatButtonModule,FormsModule,MatTooltipModule,MatGridListModule],
+  imports: [CommonModule],
   templateUrl: './exams-page.component.html',
   styleUrls: ['./exams-page.component.scss']
 })
 export class ExamsPageComponent  {
 
-  showTableHeader = false;
-  formdata={
-    examName:'',
-    totalMarks:'',
-    datepick:'',
-    time:''
-  }
-    onClick() {
-    this.showTableHeader = !this.showTableHeader;
-  }
-  onSubmit(form:NgForm){
-  }
-  
-  
-  }
-
-  // isValidTime(regex:string){
-  //   const eregex = /^(1[012]|0[1-9]):[0-5][0-9] (AM|PM)$/;
-  //   return eregex.test(this.formdata.time);
-  //   }
+}
     
   
   

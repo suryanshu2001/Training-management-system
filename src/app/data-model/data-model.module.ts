add4b24c414faa9332da67189794679412ac7a3d
vicky <vikashstd00@gmail.com>
2024-04-25 17:56:22 +0530

    worked on db.json and modal

--- src/app/data-model/data-model.module.ts
+++ src/app/data-model/data-model.module.ts
@@ -1,8 +1,7 @@
-
-export interface user{
-  email: string,
-  userName: string,
-  password: string
+export interface user {
+  email: string;
+  userName: string;
+  password: string;
 }
 
 export interface Course {
@@ -12,4 +11,45 @@ export interface Course {
 
 export interface Program {
   [programName: string]: string[];
+}
+
+export interface exams {
+  examId: string;
+  examDate: string;
+  examName: string;
+  examTime: string;
+  examTotalMarks: number;
+  examUploadFiles: string[];
+  courseName: string;
+}
+export interface Assignment {
+  assignmentId: string;
+  assignmentName: string;
+  assignmentTotalMarks: number;
+  assignmentDate: string;
+  assignmentTime: string;
+  assignmentUploadFiles: string[];
+  courseName: string;
+}
+
+export interface Students {
+  studentId: string;
+  studentName: string;
+  courses: string[];
+  marks: {
+    Examinations: { examId: string; examMarks: number }[];
+
+    Assignments: {
+      assignmentId: string;
+      AssignmentMarks: number;
+    }[];
+  };
+}
+
+interface CourseDetails {
+  code: string;
+  courseName: string;
+  topicsCompleted: string[];
+  topicsInProgress: string[];
+  courseCompletion: string;
 }

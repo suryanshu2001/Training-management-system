0ee97e439c6fbfcebbc8a01474b8c49d2988981d
Ushmita Dutta <CZ0224@cztech.com>
2024-04-25 15:55:33 +0530

    attendance component done

--- src/app/components/pages/dashboard/home-page/attendance/student-attendance/student-attendance.component.ts
+++ src/app/components/pages/dashboard/home-page/attendance/student-attendance/student-attendance.component.ts
@@ -9,5 +9,28 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./student-attendance.component.scss']
 })
 export class StudentAttendanceComponent {
+  studentNames = ['Marie', 'Jacob', 'John']; 
 
+  selectAllStudents(event: any) {
+    const allStudentsCheckbox = document.getElementById('allStudentsCheckbox') as HTMLInputElement;
+    const allStudentsChecked = allStudentsCheckbox.checked;
+    this.studentNames.forEach(student => {
+      const studentCheckbox = document.getElementById(`${student}Checkbox`) as HTMLInputElement;
+      studentCheckbox.checked = allStudentsChecked;
+    });
+  }
+  checkSingleStudent(event: any) {
+    const studentCheckbox = event.target as HTMLInputElement;
+    const studentName = studentCheckbox.id.replace('Checkbox', '');
+    const allStudentsCheckbox = document.getElementById('allStudentsCheckbox') as HTMLInputElement;
+    const allStudentsChecked = allStudentsCheckbox.checked;
+    if (!studentCheckbox.checked && allStudentsChecked) {
+      allStudentsCheckbox.checked = false;
+    } else if (studentCheckbox.checked && !this.studentNames.some(student => {
+      const studentCheckbox = document.getElementById(`${student}Checkbox`) as HTMLInputElement;
+      return !studentCheckbox.checked;
+    })) {
+      allStudentsCheckbox.checked = true;
+    }
+  }
 }
--- src/app/components/pages/dashboard/home-page/batches/batch-details/batch-details.component.ts
+++ src/app/components/pages/dashboard/home-page/batches/batch-details/batch-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,8 +10,26 @@ import { CommonModule } from '@angular/common';
 })
 export class BatchDetailsComponent {
   dataSource: any[] = [
-    { code: 234, name: 'Course 1', topicsCompleted: 'Topic 1', topicsInProgress: 'Topic 2', completionPercentage: '60%' },
-    { code: 235, name: 'Course 2', topicsCompleted: 'Topic 1', topicsInProgress: 'Topic 2', completionPercentage: '70%' },
-    { code: 236, name: 'Course 3', topicsCompleted: 'Topic 1', topicsInProgress: 'Topic 2', completionPercentage: '80%' }
+    { 
+      code: 234, 
+      name: 'Course 1', 
+      topicsCompleted: ['Topic 1', 'Topic 2', 'Topic 3', 'Topic 4', 'Topic 5','Topic 6', 'Topic 7', 'Topic 8', 'Topic 9', 'Topic 10'], 
+      topicsInProgress: 'Topic 2', 
+      completionPercentage: '60%' 
+    },
+    { 
+      code: 235, 
+      name: 'Course 2', 
+      topicsCompleted: ['Topic 6', 'Topic 7', 'Topic 8', 'Topic 9', 'Topic 10'], 
+      topicsInProgress: 'Topic 3', 
+      completionPercentage: '70%' 
+    },
+    { 
+      code: 236, 
+      name: 'Course 3', 
+      topicsCompleted: ['Topic 1', 'Topic 2'], 
+      topicsInProgress: 'Topic 2', 
+      completionPercentage: '80%' 
+    }
   ];
 }
